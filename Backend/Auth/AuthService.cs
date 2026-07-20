cab3cceab00d32b903dfb91467b4386dc697dfe6
mark-175 <152170112+mark-175@users.noreply.github.com>
2026-07-20 04:49:27 +0200

    Remove unused line

--- Backend/Auth/AuthService.cs
+++ Backend/Auth/AuthService.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
 
 namespace BookTracker.Api.Auth;
 
