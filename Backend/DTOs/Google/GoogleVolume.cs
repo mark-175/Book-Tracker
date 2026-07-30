a82bb6be114a333d9bb6b41a95540388425f6225
mark-175 <152170112+mark-175@users.noreply.github.com>
2026-07-30 16:43:13 +0200

    Remove dead duplicate GoogleVolume.ToBook mapping method

--- Backend/DTOs/Google/GoogleVolume.cs
+++ Backend/DTOs/Google/GoogleVolume.cs
@@ -1,5 +1,4 @@
 using System.Text.Json.Serialization;
-using BookTracker.Api.Entities;
 
 namespace BookTracker.Api.DTOs.Google;
 
@@ -10,24 +9,4 @@ public class GoogleVolume
 
     [JsonPropertyName("volumeInfo")]
     public GoogleVolumeInfo VolumeInfo { get; set; } = new();
-
-    public static Book ToBook(GoogleVolume volume)
-    {
-        return new Book
-        {
-            GoogleBooksId = volume.Id,
-            Title = volume.VolumeInfo.Title,
-            Subtitle = volume.VolumeInfo.Subtitle ?? string.Empty,
-            Authors = string.Join(", ", volume.VolumeInfo.Authors ?? new List<string>()),
-            Language = volume.VolumeInfo.Language ?? string.Empty,
-            Description = volume.VolumeInfo.Description,
-            CoverUrl = volume.VolumeInfo.ImageLinks?.Thumbnail,
-            Isbn10 = volume.VolumeInfo.IndustryIdentifiers?
-                .FirstOrDefault(i => i.Type == "ISBN_10")?.Identifier,
-            Isbn13 = volume.VolumeInfo.IndustryIdentifiers?
-                .FirstOrDefault(i => i.Type == "ISBN_13")?.Identifier,
-            PageCount = volume.VolumeInfo.PageCount,
-            CachedAt = DateTime.UtcNow
-        };
-    }
 }
